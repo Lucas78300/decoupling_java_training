49eb42cf3c43d6f001b8bd6906c0400737444a6a
Lucas Souillé <lsouille@et.esiea.fr>
2022-10-10 19:20:46 +0200

    [Partie 7] Add FilteredLogger class

---
+++ src/main/java/fr/lernejo/logger/FilteredLogger.java
@@ -0,0 +1,15 @@
+package fr.lernejo.logger;
+
+import java.util.function.Predicate;
+
+public class FilteredLogger implements Logger{
+
+    public FilteredLogger(Logger delegate, Predicate<String> condition) {
+        //TODO assign arguments to fields
+    }
+
+    @Override
+    public void log(String message) {
+
+    }
+}
